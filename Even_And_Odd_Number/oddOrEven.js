fe22d8a985a917ea1e15f644151c66d0e899fdcb
timmydee <dahunsitimmy@gmail.com>
2026-01-13 11:02:43 +0100

    challenge: odd_or_even

---
+++ Even_And_Odd_Number/oddOrEven.js
@@ -0,0 +1,9 @@
+function oddOrEven(n) {
+  if (n === 1) {
+    return 'Odd'
+  } else if (n % 2 === 1) {
+    return 'Odd'
+  } else {
+      return 'Even';
+  }
+}
---
+++ Even_And_Odd_Number/odd_or_even.py
@@ -0,0 +1,7 @@
+def odd_or_even(n):
+    if n == 1:
+        return 'Odd'
+    elif n % 2 == 1:
+        return 'Odd'
+    else:
+        return 'Even'
